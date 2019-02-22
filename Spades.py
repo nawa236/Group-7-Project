ff87e7610d1892d9d3672245a61bc9a641eb4657
mdtarter <43009610+mdtarter@users.noreply.github.com>
2019-02-21 22:36:58 -0500

    Text based spades,
    Printing intial plays cards work. Implemented classes and functions.
    Need to work on actual game code next

---
+++ Spades.py
@@ -0,0 +1,59 @@
+
+#text based spades game
+from cardDeck import *
+
+
+class Player():
+    def __init__(self, score = 0,):
+        self.score = score
+        self.hand = []
+
+    def set_bet(self, bet):
+        self.bet = bet
+
+    def add(self,x):
+        self.hand.append(x)
+
+    def print_hand(self):
+        for i in range(len(self.hand)-1):
+            print_card(self.hand[i])
+            print(', ', end="", flush=True)
+        print_card(self.hand[i])
+        print('')
+
+
+
+
+def deal_cards(deck, player, comp1, comp2, comp3):
+    for i in range(13):
+        player.add(draw_card(deck))
+        comp1.add(draw_card(deck))
+        comp2.add(draw_card(deck))
+        comp3.add(draw_card(deck))
+    return
+
+
+running = True
+
+while running:
+    play = input("Would you like to play Spades? y or n:  ")
+    if play == 'y':
+        playerName = input("What is your name?  ")
+        #deal cards ask user for how many hands
+        #create deck
+        newdeck = create_deck()
+        Player1 = Player()
+        Teammate = Player()
+        Computer1 = Player()
+        Computer2 = Player()
+        deal_cards(newdeck,Player1, Teammate, Computer1, Computer2 )
+
+        print(playerName,"'s Cards:")
+        Player1.print_hand()
+
+    elif play == 'n':
+        print('Thanks for playing spades')
+        running = False
+    else:
+        print('Please enter y or n')
+    
--- cardDeck.py
+++ cardDeck.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from enum import IntEnum
+from random import *
 
 full_deck = []
 
@@ -26,16 +27,36 @@ class Suit(Enum):
 
 class PlayingCard:
     def __init__(self, card_rank, card_suit):
-        self.card = card_rank;
-        self.card = card_suit;
+        self.card = card_rank
+        self.suit = card_suit
 
 def create_deck():
     for suit in Suit :
         for card in Card:
-            full_deck.append(PlayingCard(Card(card), Suit(suit)))
+            full_deck.append(PlayingCard(Card(card), Suit(suit)))        
     return full_deck
 
 def draw_card(deck):
     rand_card = randint(0, len(deck)-1)
     return deck.pop(rand_card)
 
+def print_card(card):
+    if card.card < 11:
+        print(card.card.value, end="", flush=True)
+    elif card.card == 11:
+        print('J', end="", flush=True)
+    elif card.card == 12:
+        print('Q', end="", flush=True)
+    elif card.card == 13:
+        print('K', end="", flush=True)
+    elif card.card == 14:
+        print('A', end="", flush=True)
+    if card.suit.value == 'spades':
+        print('♠', end="", flush=True)
+    elif card.suit.value == 'diamonds':
+        print('♦', end="", flush=True)
+    elif card.suit.value == 'hearts':
+        print('♥', end="", flush=True)
+    else:
+        print('♣', end="", flush=True)
+    return
