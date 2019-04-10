e296c308f5a4491408497d09b13bd2f78dabc9d0
nawa236 <nawa236@uky.edu>
2019-04-10 09:43:33 -0400

    Fixed Card Deck

--- project/cardDeck.py
+++ project/cardDeck.py
@@ -30,114 +30,114 @@ class PlayingCard:
     def __init__(self, card_rank, card_suit):
         self.card = card_rank
         self.suit = card_suit
-        if card.suit.value == 'hearts':
-            if card.card == 2:
-                cardImg = pygame.image.load('.\JPEG\\2H.jpg')
-            elif card.card == 3:
-                cardImg = pygame.image.load('.\JPEG\\3H.jpg')
-            elif card.card == 4:
-                cardImg = pygame.image.load('.\JPEG\\4H.jpg')
-            elif card.card == 5:
-                cardImg = pygame.image.load('.\JPEG\\5H.jpg')
-            elif card.card == 6:
-                cardImg = pygame.image.load('.\JPEG\\6H.jpg')
-            elif card.card == 7:
-                cardImg = pygame.image.load('.\JPEG\\7H.jpg')
-            elif card.card == 8:
-                cardImg = pygame.image.load('.\JPEG\8H.jpg')
-            elif card.card == 9:
-                cardImg = pygame.image.load('.\JPEG\9H.jpg')
-            elif card.card == 10:
-                cardImg = pygame.image.load('.\JPEG\\10H.jpg')
-            elif card.card == 11:
-                cardImg = pygame.image.load('.\JPEG\JH.jpg')
-            elif card.card == 12:
-                cardImg = pygame.image.load('.\JPEG\QH.jpg')
-            elif card.card == 13:
-                cardImg = pygame.image.load('.\JPEG\KH.jpg')
-            elif card.card == 14:
-                cardImg = pygame.image.load('.\JPEG\AH.jpg')
-        elif card.suit.value == 'diamonds':
-            if card.card == 2:
-                cardImg = pygame.image.load('.\JPEG\\2D.jpg')
-            elif card.card == 3:
-                cardImg = pygame.image.load('.\JPEG\\3D.jpg')
-            elif card.card == 4:
-                cardImg = pygame.image.load('.\JPEG\\4D.jpg')
-            elif card.card == 5:
-                cardImg = pygame.image.load('.\JPEG\\5D.jpg')
-            elif card.card == 6:
-                cardImg = pygame.image.load('.\JPEG\\6D.jpg')
-            elif card.card == 7:
-                cardImg = pygame.image.load('.\JPEG\\7D.jpg')
-            elif card.card == 8:
-                cardImg = pygame.image.load('.\JPEG\8D.jpg')
-            elif card.card == 9:
-                cardImg = pygame.image.load('.\JPEG\9D.jpg')
-            elif card.card == 10:
-                cardImg = pygame.image.load('.\JPEG\\10D.jpg')
-            elif card.card == 11:
-                cardImg = pygame.image.load('.\JPEG\JD.jpg')
-            elif card.card == 12:
-                cardImg = pygame.image.load('.\JPEG\QD.jpg')
-            elif card.card == 13:
-                cardImg = pygame.image.load('.\JPEG\KD.jpg')
-            elif card.card == 14:
-                cardImg = pygame.image.load('.\JPEG\AD.jpg')
-        elif card.suit.value == 'spades':
-            if card.card == 2:
-                cardImg = pygame.image.load('.\JPEG\\2S.jpg')
-            elif card.card == 3:
-                cardImg = pygame.image.load('.\JPEG\\3S.jpg')
-            elif card.card == 4:
-                cardImg = pygame.image.load('.\JPEG\\4S.jpg')
-            elif card.card == 5:
-                cardImg = pygame.image.load('.\JPEG\\5S.jpg')
-            elif card.card == 6:
-                cardImg = pygame.image.load('.\JPEG\\6S.jpg')
-            elif card.card == 7:
-                cardImg = pygame.image.load('.\JPEG\\7S.jpg')
-            elif card.card == 8:
-                cardImg = pygame.image.load('.\JPEG\8S.jpg')
-            elif card.card == 9:
-                cardImg = pygame.image.load('.\JPEG\9S.jpg')
-            elif card.card == 10:
-                cardImg = pygame.image.load('.\JPEG\\10S.jpg')
-            elif card.card == 11:
-                cardImg = pygame.image.load('.\JPEG\JS.jpg')
-            elif card.card == 12:
-                cardImg = pygame.image.load('.\JPEG\QS.jpg')
-            elif card.card == 13:
-                cardImg = pygame.image.load('.\JPEG\KS.jpg')
-            elif card.card == 14:
-                cardImg = pygame.image.load('.\JPEG\AS.jpg')
-        elif card.suit.value == 'clubs':
-            if card.card == 2:
-                cardImg = pygame.image.load('.\JPEG\\2C.jpg')
-            elif card.card == 3:
-                cardImg = pygame.image.load('.\JPEG\\3C.jpg')
-            elif card.card == 4:
-                cardImg = pygame.image.load('.\JPEG\\4C.jpg')
-            elif card.card == 5:
-                cardImg = pygame.image.load('.\JPEG\\5C.jpg')
-            elif card.card == 6:
-                cardImg = pygame.image.load('.\JPEG\\6C.jpg')
-            elif card.card == 7:
-                cardImg = pygame.image.load('.\JPEG\\7C.jpg')
-            elif card.card == 8:
-                cardImg = pygame.image.load('.\JPEG\8C.jpg')
-            elif card.card == 9:
-                cardImg = pygame.image.load('.\JPEG\9C.jpg')
-            elif card.card == 10:
-                cardImg = pygame.image.load('.\JPEG\\10C.jpg')
-            elif card.card == 11:
-                cardImg = pygame.image.load('.\JPEG\JC.jpg')
-            elif card.card == 12:
-                cardImg = pygame.image.load('.\JPEG\QC.jpg')
-            elif card.card == 13:
-                cardImg = pygame.image.load('.\JPEG\KC.jpg')
-            elif card.card == 14:
-                cardImg = pygame.image.load('.\JPEG\AC.jpg')
+        if card_suit.value == 'hearts':
+            if card_rank == 2:
+                self.cardImg = pygame.image.load('.\JPEG\\2H.jpg')
+            elif card_rank == 3:
+                self.cardImg = pygame.image.load('.\JPEG\\3H.jpg')
+            elif card_rank == 4:
+                self.cardImg = pygame.image.load('.\JPEG\\4H.jpg')
+            elif card_rank == 5:
+                self.cardImg = pygame.image.load('.\JPEG\\5H.jpg')
+            elif card_rank == 6:
+                self.cardImg = pygame.image.load('.\JPEG\\6H.jpg')
+            elif card_rank == 7:
+                self.cardImg = pygame.image.load('.\JPEG\\7H.jpg')
+            elif card_rank == 8:
+                self.cardImg = pygame.image.load('.\JPEG\8H.jpg')
+            elif card_rank == 9:
+                self.cardImg = pygame.image.load('.\JPEG\9H.jpg')
+            elif card_rank == 10:
+                self.cardImg = pygame.image.load('.\JPEG\\10H.jpg')
+            elif card_rank == 11:
+                self.cardImg = pygame.image.load('.\JPEG\JH.jpg')
+            elif card_rank == 12:
+                self.cardImg = pygame.image.load('.\JPEG\QH.jpg')
+            elif card_rank == 13:
+                self.cardImg = pygame.image.load('.\JPEG\KH.jpg')
+            elif card_rank == 14:
+                self.cardImg = pygame.image.load('.\JPEG\AH.jpg')
+        elif card_suit.value == 'diamonds':
+            if card_rank == 2:
+                self.cardImg = pygame.image.load('.\JPEG\\2D.jpg')
+            elif card_rank == 3:
+                self.cardImg = pygame.image.load('.\JPEG\\3D.jpg')
+            elif card_rank == 4:
+                self.cardImg = pygame.image.load('.\JPEG\\4D.jpg')
+            elif card_rank == 5:
+                self.cardImg = pygame.image.load('.\JPEG\\5D.jpg')
+            elif card_rank == 6:
+                self.cardImg = pygame.image.load('.\JPEG\\6D.jpg')
+            elif card_rank == 7:
+                self.cardImg = pygame.image.load('.\JPEG\\7D.jpg')
+            elif card_rank == 8:
+                self.cardImg = pygame.image.load('.\JPEG\8D.jpg')
+            elif card_rank == 9:
+                self.cardImg = pygame.image.load('.\JPEG\9D.jpg')
+            elif card_rank == 10:
+                self.cardImg = pygame.image.load('.\JPEG\\10D.jpg')
+            elif card_rank == 11:
+                self.cardImg = pygame.image.load('.\JPEG\JD.jpg')
+            elif card_rank == 12:
+                self.cardImg = pygame.image.load('.\JPEG\QD.jpg')
+            elif card_rank == 13:
+                self.cardImg = pygame.image.load('.\JPEG\KD.jpg')
+            elif card_rank == 14:
+                self.cardImg = pygame.image.load('.\JPEG\AD.jpg')
+        elif card_suit.value == 'spades':
+            if card_rank == 2:
+                self.cardImg = pygame.image.load('.\JPEG\\2S.jpg')
+            elif card_rank == 3:
+                self.cardImg = pygame.image.load('.\JPEG\\3S.jpg')
+            elif card_rank == 4:
+                self.cardImg = pygame.image.load('.\JPEG\\4S.jpg')
+            elif card_rank == 5:
+                self.cardImg = pygame.image.load('.\JPEG\\5S.jpg')
+            elif card_rank == 6:
+                self.cardImg = pygame.image.load('.\JPEG\\6S.jpg')
+            elif card_rank == 7:
+                self.cardImg = pygame.image.load('.\JPEG\\7S.jpg')
+            elif card_rank == 8:
+                self.cardImg = pygame.image.load('.\JPEG\8S.jpg')
+            elif card_rank == 9:
+                self.cardImg = pygame.image.load('.\JPEG\9S.jpg')
+            elif card_rank == 10:
+                self.cardImg = pygame.image.load('.\JPEG\\10S.jpg')
+            elif card_rank == 11:
+                self.cardImg = pygame.image.load('.\JPEG\JS.jpg')
+            elif card_rank == 12:
+                self.cardImg = pygame.image.load('.\JPEG\QS.jpg')
+            elif card_rank == 13:
+                self.cardImg = pygame.image.load('.\JPEG\KS.jpg')
+            elif card_rank == 14:
+                self.cardImg = pygame.image.load('.\JPEG\AS.jpg')
+        elif card_suit.value == 'clubs':
+            if card_rank == 2:
+                self.cardImg = pygame.image.load('.\JPEG\\2C.jpg')
+            elif card_rank == 3:
+                self.cardImg = pygame.image.load('.\JPEG\\3C.jpg')
+            elif card_rank == 4:
+                self.cardImg = pygame.image.load('.\JPEG\\4C.jpg')
+            elif card_rank == 5:
+                self.cardImg = pygame.image.load('.\JPEG\\5C.jpg')
+            elif card_rank == 6:
+                self.cardImg = pygame.image.load('.\JPEG\\6C.jpg')
+            elif card_rank == 7:
+                self.cardImg = pygame.image.load('.\JPEG\\7C.jpg')
+            elif card_rank == 8:
+                self.cardImg = pygame.image.load('.\JPEG\8C.jpg')
+            elif card_rank == 9:
+                self.cardImg = pygame.image.load('.\JPEG\9C.jpg')
+            elif card_rank == 10:
+                self.cardImg = pygame.image.load('.\JPEG\\10C.jpg')
+            elif card_rank == 11:
+                self.cardImg = pygame.image.load('.\JPEG\JC.jpg')
+            elif card_rank == 12:
+                self.cardImg = pygame.image.load('.\JPEG\QC.jpg')
+            elif card_rank == 13:
+                self.cardImg = pygame.image.load('.\JPEG\KC.jpg')
+            elif card_rank == 14:
+                self.cardImg = pygame.image.load('.\JPEG\AC.jpg')
 
 def create_deck():
     for suit in Suit :
@@ -160,16 +160,16 @@ def print_card(card):
         print('K', end="", flush=True)
     elif card.card == 14:
         print('A', end="", flush=True)
-    if card.suit.value == 'spades':
+    if card.suit == 'spades':
         print('♠', end="", flush=True)
-    elif card.suit.value == 'diamonds':
+    elif card.suit == 'diamonds':
         print('♦', end="", flush=True)
-    elif card.suit.value == 'hearts':
+    elif card.suit == 'hearts':
         print('♥', end="", flush=True)
     else:
         print('♣', end="", flush=True)
     return
 
 def disp_card(screen, card, x, y):
-    cardImg = pygame.transform.scale(cardImg, (50, 100))
-    screen.blit(cardImg, (x,y))
+    card.cardImg = pygame.transform.scale(card.cardImg, (50, 100))
+    screen.blit(card.cardImg, (x,y))
